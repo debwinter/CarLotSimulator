72ee6670db8838ce8ee6d148a35df423be48c2c1
Debra Winter <deb@debwinter.com>
2024-01-25 17:50:21 -0500

    updated for static exercise in module 3

--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
-	public class Carlot
+	public class CarLot
 	{
-		public Carlot()
+		public CarLot()
 		{
 		}
 
 		public List<Car> carLot { get; set; }
+
+		public static int numberOfCars = 0;
 
 	}
 }
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -7,22 +7,10 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            List<Car> carLot = new List<Car>();
+            CarLot lot1 = new CarLot();
+            lot1.carLot = new List<Car>();
 
-            //TODO
-
-            //Create a separate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
-
-
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-
-            //*************BONUS*************//
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
-
+            //adding car1 using dot notation
             Car car1 = new Car();
             car1.Year = 2006;
             car1.Make = "Dodge";
@@ -31,8 +19,13 @@ namespace CarLotSimulator
             car1.HonkNoise = "HONK";
             car1.isDriveable = true;
 
-            carLot.Add(car1);
+            //calling methods and parking it in the lot (as per static exercise)
+            car1.MakeEngineNoise(car1.EngineNoise);
+            car1.MakeHonkNoise(car1.HonkNoise);
+            ParkCar(car1, lot1.carLot);
+            Console.WriteLine();
 
+            //adding car2 using object initializer syntax
             Car car2 = new Car()
             {
                 Year = 2007,
@@ -43,49 +36,57 @@ namespace CarLotSimulator
                 isDriveable = false
             };
 
-            carLot.Add(car2);
+            //calling methods and parking it in the lot (as per static exercise)
+            car2.MakeEngineNoise(car2.EngineNoise);
+            car2.MakeHonkNoise(car2.HonkNoise);
+            ParkCar(car2, lot1.carLot);
+            Console.WriteLine();
 
+            //adding car3 using custom constructor #1
             Car car3 = new Car(2012, "Dodge", "Ram");
             car3.isDriveable = true;
 
-            carLot.Add(car3);
-
-            Car car4 = new Car(2023, "Mercedes", "Sprinter", "zoom", "boop", true);
-
-            carLot.Add(car4);
-
-            //Call each of the methods for each car
-
-            car1.MakeEngineNoise(car1.EngineNoise);
-            car1.MakeHonkNoise(car1.HonkNoise);
-            Console.WriteLine();
-            car2.MakeEngineNoise(car2.EngineNoise);
-            car2.MakeHonkNoise(car2.HonkNoise);
-            Console.WriteLine();
+            //calling methods and parking it in the lot (as per static exercise)
             car3.MakeEngineNoise(car3.EngineNoise);
             car3.MakeHonkNoise(car3.HonkNoise);
+            ParkCar(car3, lot1.carLot);
             Console.WriteLine();
+
+            //adding car4 using custom constructor #2
+            Car car4 = new Car(2023, "Mercedes", "Sprinter", "zoom", "boop", true);
+
+            //calling methods and parking it in the lot (as per static exercise)
             car4.MakeEngineNoise(car4.EngineNoise);
             car4.MakeHonkNoise(car4.HonkNoise);
-
+            ParkCar(car4, lot1.carLot);
             Console.WriteLine();
-            Console.WriteLine();
+            
             Console.WriteLine();
 
-            //*************BONUS X 2*************//
-
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            //Bonus exercise from original assignment
 
             int i = 1;
-            foreach (Car car in carLot)
+            foreach (Car car in lot1.carLot)
             {
                 Console.WriteLine($"PARKING SPACE #{i}:");
                 Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
                 Console.WriteLine();
                 i++;
+            }
+        }
+
+        //Method for static exercise
+        static void ParkCar(Car car, List<Car> list)
+        {
+            list.Add(car);
+            CarLot.numberOfCars++;
+            if (CarLot.numberOfCars == 1)
+            {
+                Console.WriteLine($"There is now {CarLot.numberOfCars} car parked in the lot.");
+            }
+            else
+            {
+                Console.WriteLine($"There are now {CarLot.numberOfCars} cars parked in the lot.");
             }
         }
     }
